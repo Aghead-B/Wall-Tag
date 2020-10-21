03f2e7f1a37147177142250f6d2944fa771fffef
M.A. Bilal <aghead.bilal@hva.nl>
2020-10-21 23:44:38 +0200

    Upload New File

---
+++ game.py
@@ -0,0 +1,26 @@
+# score = 0
+# target = bool()
+# while target
+
+target = bool()
+
+import time
+while True:
+    print("This prints once a minute.")
+    time.sleep(0.10) # Delay for 1 minute (60 seconds).
+    if 
+
+
+
+i = 0
+levens = 3
+while i < 3:
+    i+= 1
+    if i == 2:
+        print("Levens: " ,levens - 1)
+
+
+
+
+
+
