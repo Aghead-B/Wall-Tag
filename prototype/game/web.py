2c8de4287382b042dc736d2091c37d9c0cc090f3
Evy Dekkers <17366381+evyd13@users.noreply.github.com>
2020-10-25 13:22:32 +0100

    update game

--- prototype/game/web.py
+++ prototype/game/web.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
-import Game
+from Game import Game
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    game = Game(True)
+    return render_template('index.html', variabele=game.playerLives, levens=game.GUN_DISTANCE_SAMPLES)
+
+@app.route('/test')
+def test():
+    print("test")
     return render_template('index.html')
 
 
