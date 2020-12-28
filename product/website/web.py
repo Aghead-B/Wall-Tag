c330b26682e59664d292d97add6b761ee060c471
app3 <hamza.ben.ali@hva.nl>
2020-12-28 18:12:45 +0100

    da new change

--- product/website/web.py
+++ product/website/web.py
@@ -1,24 +1,62 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from Game import Game
-app = Flask(__name__, static_url_path='/static')
+
+
+app: Flask = Flask(__name__, static_url_path='/static')
 game = Game(True)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/signup', methods = ['POST'])
+@app.route('/signup', methods=['POST'])
 def signup():
-    player_name = request.form['player_name']
-    game.playerName = player_name
+    game.playerName = request.form['player_name']
+    if not game.playerName:
+        return redirect('/')
+    return redirect('/play')
+
+
+@app.route('/get_data')
+def get_data():
+    return jsonify(playerName = game.playerName, playerLives=game.playerLives)
+
+
+@app.route('/play')
+def play():
+    return render_template('update.html')
+
+
+@app.route('/start')
+def start():
+    print("test start")
     game.start()
-    return render_template('game.html')
+    return ''
 
-@app.route('/update')
-def update():
-    return render_template('update.html', game=game)
 
+@app.route('/quit')
+def quit():
+    print("test quit")
+    game.quit()
+    return ''
+
+
+@app.route("/status")
+def return_stat():
+    return jsonify(targetStatus = game.targetStatus)
+
+
+def return_lastline():
+    lastline = game.lastline
+    print(lastline)
+    return jsonify(lastline)
+
+
+@app.context_processor
+def context_processor():
+    return dict(lastline=return_lastline, return_stat=return_stat)
 
 
 if __name__ == "__main__":
